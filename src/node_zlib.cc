c39ab3a38aae04c8a4367df006eef4538468d899
isaacs <i@izs.me>
2011-09-08 17:46:25 -0700

    Doing deflate synchronously and slowly

--- src/node_zlib.cc
+++ src/node_zlib.cc
@@ -42,48 +42,164 @@ namespace node {
 
 using namespace v8;
 
+static Persistent<String> ondata_sym;
+static Persistent<String> onend_sym;
+
+const char * zlib_perr(int code)
+{
+  switch (code) {
+    case Z_ERRNO        : return "Z_ERRNO";
+    case Z_STREAM_ERROR : return "Z_STREAM_ERROR";
+    case Z_DATA_ERROR   : return "Z_DATA_ERROR";
+    case Z_MEM_ERROR    : return "Z_MEM_ERROR";
+    case Z_BUF_ERROR    : return "Z_BUF_ERROR";
+    case Z_VERSION_ERROR: return "Z_VERSION_ERROR";
+    default             : return "Unknown Error";
+  }
+}
 
 
-class Zlib : public ObjectWrap {
+class Deflate : public ObjectWrap {
 
  public:
 
-  Zlib() : ObjectWrap() {
-    Init();
+  Deflate(int level) : ObjectWrap() {
+    Init(level);
   }
 
-  ~Zlib() {
+  ~Deflate() {
+  }
+
+  static Handle<Value> Write(const Arguments& args) {
+    size_t buffer_length;
+    Bytef * buf;
+    if (args.Length() < 1) {
+      // just a flush or end call
+      buf = (Bytef *)"\0";
+      buffer_length = 0;
+    } else {
+      if (!Buffer::HasInstance(args[0])) {
+        return ThrowException(Exception::Error(
+                    String::New("First argument needs to be a buffer")));
+      }
+
+      Local<Object> buffer_obj = args[0]->ToObject();
+      buf = (Bytef *)Buffer::Data(buffer_obj);
+      buffer_length = Buffer::Length(buffer_obj);
+    }
+
+    Deflate *self = ObjectWrap::Unwrap<Deflate>(args.This());
+
+    self->strm.avail_in = buffer_length;
+    self->strm.next_in = buf;
+
+    do {
+      self->strm.avail_out = CHUNK;
+      self->strm.next_out = self->out;
+
+      // XXX Do this in a thread.
+      // Also, buffer up pending writes, and return false if
+      // there are more in the queue.
+      self->err = deflate(&(self->strm), self->flush);    /* no bad return value */
+      assert(self->err != Z_STREAM_ERROR);  /* state not clobbered */
+
+      self->have = CHUNK - self->strm.avail_out;
+      Buffer* deflated = Buffer::New((char *)(self->out), self->have);
+      if (self->handle_->Has(ondata_sym)) {
+        Handle<Value> od = self->handle_->Get(ondata_sym);
+        assert(od->IsFunction());
+        Handle<Function> ondata = Handle<Function>::Cast(od);
+        Handle<Value> odargv[1] = { deflated->handle_ };
+        ondata->Call(self->handle_, 1, odargv);
+      }
+
+    } while (self->strm.avail_out == 0);
+
+    assert(self->strm.avail_in == 0);     /* all input will be used */
+
+    return True();
+  }
+
+  static Handle<Value> End(const Arguments& args) {
+    Deflate *self = ObjectWrap::Unwrap<Deflate>(args.This());
+
+    self->flush = Z_FINISH;
+    if ( args.Length() >= 1 ) {
+      self->Write(args);
+    }
+    if (self->handle_->Has(onend_sym)) {
+      Handle<Value> oe = self->handle_->Get(onend_sym);
+      assert(oe->IsFunction());
+      Handle<Function> onend = Handle<Function>::Cast(oe);
+      onend->Call(self->handle_, 0, NULL);
+    }
+    return True();
   }
 
   static Handle<Value> New(const Arguments& args) {
     HandleScope scope;
 
-    Zlib *self;
+    Deflate *self;
+
+    int level_ = args[0]->Int32Value();
+    if (level_ < -1 || level_ > 9) {
+      return ThrowException(Exception::Error(
+            String::New("Invalid compression level")));
+    }
+
+    self = new Deflate(level_);
+    if (self->err != Z_OK) {
+      return ThrowException(Exception::Error(
+            String::New(zlib_perr(self->err))));
+    }
+
     self->Wrap(args.This());
 
     return args.This();
   }
 
+
  private:
 
-  void Init () {
-  }
-
+  int err;
+  int level;
   z_stream strm;
+  int flush;
+
+  unsigned char out[CHUNK];
+  unsigned have;
+
+  void Init (int level_) {
+    level = level_;
+    strm.zalloc = Z_NULL;
+    strm.zfree = Z_NULL;
+    strm.opaque = Z_NULL;
+    err = deflateInit(&strm, level);
+    flush = Z_NO_FLUSH;
+    if (err != Z_OK) {
+      return;
+    }
+  }
 };
 
 
 void InitZlib(Handle<Object> target) {
   HandleScope scope;
 
-  Local<FunctionTemplate> t = FunctionTemplate::New(Zlib::New);
+  Local<FunctionTemplate> t = FunctionTemplate::New(Deflate::New);
 
   // zstrm struct
-  t->InstanceTemplate()->SetInternalFieldCount(1);
+  t->InstanceTemplate()->SetInternalFieldCount(4);
 
-  t->SetClassName(String::NewSymbol("Zlib"));
+  NODE_SET_PROTOTYPE_METHOD(t, "write", Deflate::Write);
+  NODE_SET_PROTOTYPE_METHOD(t, "end", Deflate::End);
 
-  target->Set(String::NewSymbol("Zlib"), t->GetFunction());
+  t->SetClassName(String::NewSymbol("Deflate"));
+
+  target->Set(String::NewSymbol("Deflate"), t->GetFunction());
+
+  ondata_sym = NODE_PSYMBOL("onData");
+  onend_sym = NODE_PSYMBOL("onEnd");
 }
 
 }  // namespace node
--- test/simple/test-zlib.js
+++ test/simple/test-zlib.js
@@ -1,3 +1,20 @@
 // nothing here yet, just make sure it loads.
 
 var z = require('zlib');
+var df = new z.Deflate(-1);
+
+df.onData = function (c) {
+  console.error('onData', c.toString("hex"));
+};
+df.onEnd = function () {
+  console.error('onEnd');
+};
+
+df.write(new Buffer("hello"))
+df.write(new Buffer(" world"))
+df.write(new Buffer(" world"))
+df.write(new Buffer(" world"))
+df.write(new Buffer(" world"))
+df.write(new Buffer(" world"))
+df.write(new Buffer(" world"))
+df.end(new Buffer("!"))
